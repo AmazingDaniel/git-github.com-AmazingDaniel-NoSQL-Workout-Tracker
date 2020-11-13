ad216b11fc907006ff23352d51533f1a9e66c31f
Daniel Moreno <drmjr417@sbcglobal.net>
2020-11-12 23:49:25 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const mongoose = require ("mongoose")
+const express = require ("express")
+const apps = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+  });
+
+apps.use(express.urlencoded({extended:true}));
+apps.use(express.json());
+apps.use(express.static("public"));
+
+apps.use(require("./route/htmlRoute"));
+apps.use(require("./route/apiRoute"));
+
+apps.listen(PORT, () => {
+    console.log(`Application run on port ${PORT}`);
+});
