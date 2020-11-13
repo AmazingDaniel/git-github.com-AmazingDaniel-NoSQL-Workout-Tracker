92bd985acf8e22bee12b3f6529c77ade27dc7403
Daniel Moreno <drmjr417@sbcglobal.net>
2020-11-13 15:04:24 -0600

    console.log

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const express = require ("express")
 const apps = express();
 const PORT = process.env.PORT || 3000;
 
+console.log(process.env.MONGODB_URI)
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
     useFindAndModify: false
   });
+
 
 apps.use(express.urlencoded({extended:true}));
 apps.use(express.json());
